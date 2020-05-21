a5780f6f9441e811ad8e813ceaad48f4c604fa4d
ekoprasetyo98 <masukpak33@gmail.com>
2020-05-21 12:58:33 +0700

    first commit

---
+++ app/src/main/java/com/example/training/model/TrainingModel.java
@@ -0,0 +1,37 @@
+package com.example.training.model;
+
+import java.util.Collection;
+
+public class TrainingModel {
+    public String getStatusTraining() {
+        return null;
+    }
+
+    public int getNamaTraining() {
+        return 0;
+    }
+
+    public int getTypeTraining() {
+        return 0;
+    }
+
+    public int getJumlahTraining() {
+        return 0;
+    }
+
+    public int getHargaTraining() {
+        return 0;
+    }
+
+    public int getTanggalTraining() {
+        return 0;
+    }
+
+    public byte[] getGambarTraining() {
+        return new byte[0];
+    }
+
+    public String getIdTraining() {
+        return null;
+    }
+}
