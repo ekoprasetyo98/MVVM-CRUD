ba460bd23fbe6e129e666428445f866355b22c3b
ekoprasetyo98 <masukpak33@gmail.com>
2020-05-28 11:28:10 +0700

    second commit

--- app/src/main/java/com/example/training/rest/ApiConfig.java
+++ app/src/main/java/com/example/training/rest/ApiConfig.java
@@ -6,7 +6,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class ApiConfig {
     public static ApiService getApiService(){
         Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("http://192.168.43.101/eko/lesKohDerry/")
+                .baseUrl("http://192.168.43.76/~mac/lesKohDerry/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         ApiService service =retrofit.create(ApiService.class);
